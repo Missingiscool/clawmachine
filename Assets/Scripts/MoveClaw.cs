063f5a3c1d681b0020883506a590ed51c788d79a
Andrew <missinggaminginc@gmail.com>
2021-12-19 12:37:30 -0500

    test text

--- Assets/Scripts/MoveClaw.cs
+++ Assets/Scripts/MoveClaw.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoveClaw : MonoBehaviour
 {
@@ -10,7 +11,8 @@ public class MoveClaw : MonoBehaviour
     private float horizontalInput;
     private float verticalInput;
 
-    public int position;
+    public int state;
+    public Text text;
     private bool clawable;
     private Vector3 startPos;
 
@@ -18,12 +20,13 @@ public class MoveClaw : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        position = 0;
+        text.text = "Start";
+        state = 0;
         clawable = true;
         startPos = transform.position;
     }
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
         horizontalInput = Input.GetAxis("Horizontal");
@@ -34,39 +37,39 @@ public class MoveClaw : MonoBehaviour
         if (Input.GetKey(KeyCode.Space) && clawable)
         {
             clawable = false;
-            position = 1;
+            state = 1;
         }
 
         //drop 1
-        if(position == 1 && !clawable)
+        if(state == 1 && !clawable)
         {
             transform.position += Vector3.down * Time.deltaTime * dropSpeed;
-            print("claw drop now");
+            printText("claw drop now");
         }
         //close claw 2
-        if (position == 2 && !clawable)
+        if (state == 2 && !clawable)
         {
-            print("claw close now");
-            position = 3;
+            printText("claw close now");
+            CloseClaw();
         }
         //raise claw
-        if (position == 3 && !clawable)
+        if (state == 3 && !clawable)
         {
-            print("claw lifting now");
+            printText("claw lifting now");
             if (transform.position.y < startPos.y)
             {
                 transform.position += Vector3.up * Time.deltaTime * dropSpeed;
-                print(startPos.y.ToString() + " " + transform.position.y.ToString());
+                //print(startPos.y.ToString() + " " + transform.position.y.ToString());
             }
             else
             {
-                position = 4;
+                state = 4;
             }
         }
         //move claw back to start
-        if (position == 4 && !clawable)
+        if (state == 4 && !clawable)
         {
-            print("claw moving back to starting position");
+            printText("claw moving back to starting position");
             if (transform.position.x > startPos.x)
             {
                 transform.position += Vector3.left * Time.deltaTime * speed;
@@ -79,16 +82,14 @@ public class MoveClaw : MonoBehaviour
             }
             else
             {
-                position = 5;
+                state = 5;
             }
         }
         //open claw
-        if (position == 5 && !clawable)
+        if (state == 5 && !clawable)
         {
-            print("claw opening now");
-
-            position = 0;
-            clawable = true;
+            printText("claw opening now");
+            OpenClaw();
         }
 
         if (clawable) { 
@@ -104,41 +105,48 @@ public class MoveClaw : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        print("collision detected");
-        if(position == 1)
+        printText("collision detected");
+        if(state == 1)
         {
-            position = 2;
+            state = 2;
         }
     }
     private void DropClaw()
     {
         //drop claw down
-        print("claw dropped");
+        printText("claw dropped");
         CloseClaw();
     }
 
     private void CloseClaw()
     {
         //close claw
-        print("claw closed");
-        LiftClaw();
+        printText("claw closed");
+        state = 3;
+        //LiftClaw();
     }
 
     private void LiftClaw()
     {
-        print("claw lifted");
+        printText("claw lifted");
         ResetPosition();
     }
 
     private void ResetPosition()
     {
-        print("claw back to starting position");
+        printText("claw back to starting position");
         OpenClaw();
     }
 
     private void OpenClaw()
     {
-        print("claw opened");
+        printText("claw opened");
+        state = 0;
         clawable = true;
+    }
+
+    private void printText(string s)
+    {
+        text.text = s;
     }
 }
