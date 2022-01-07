064bb14b29aa7a9e4f8876ef43f42c983d34febf
Brittany <brittany.balcom@gmail.com>
2022-01-07 17:54:05 -0500

    Made prize chute larger, played around with move script, added squirrel

---
+++ Assets/move.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(Vector3.forward * Time.deltaTime * 3);
+        }    
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(Vector3.back * Time.deltaTime * 3);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(Vector3.left * Time.deltaTime * 3);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(Vector3.right * Time.deltaTime * 3);
+        }
+    }
+}
